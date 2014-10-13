8ba764e62dfe2ea925ec071613abc95b698940b9
Yang <ipv6.elata@gmail.com>
2014-10-13 22:42:16 +0800

    初始化项目

---
+++ Gruntfile.js
@@ -0,0 +1,18 @@
+/**
+ * Created by yang on 14-10-13.
+ */
+
+module.exports = function(grunt){
+    grunt.initConfig({
+        nodewebkit: {
+            options: {
+                platforms: ['win','osx'],
+                buildDir: './builds', // Where the build version of my node-webkit app is saved
+            },
+            src: ['./src/**/*'] // Your node-webkit app
+        }
+    });
+    grunt.loadNpmTasks('grunt-node-webkit-builder');
+
+    grunt.registerTask('default', ['nodewebkit']);
+};
--- README.md
+++ README.md
@@ -2,3 +2,20 @@ open3dhub
 =========
 
 一套开源的3d资源分享工具
+
+本软件采用node-webkit开发，
+
+运行方式：
+
+npm install
+
+nw src
+
+node-webkit在mac上的开发配置参考[这篇文章](http://yuankeqiang.lofter.com/post/8de51_a50fee)
+
+
+打包发布：
+
+npm install grunt-cli -g
+
+npm install & grunt
